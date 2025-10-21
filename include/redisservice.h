059d04b8743578eddb269baeba988f4ec861b71b
QUBO <346386694@qq.com>
2025-10-21 19:19:58 +0800

    first commit

--- include/redisservice.h
+++ include/redisservice.h
@@ -3,13 +3,37 @@
 #define DLL_EXPORTS extern "C" __declspec(dllexport)
 #define DLL_CLASSEXP __declspec(dllexport)
 #include "dataStructure.h"
+#include <vector>
 
 class redisserviceParam;
+
+class DLL_CLASSEXP redisType
+{
+public:
+	redisType() {};
+	~redisType() {};
+	void setvalue(std::string str) { s_value = str; };
+
+
+	double toDouble();
+	int toInt();
+	std::string toString();
+	bool toBool();
+private:
+	std::string s_value;
+};
 
 class DLL_CLASSEXP redisservice
 {
 public:
 	static redisservice *instance();
+
+	void setValue(const char *key,const char *value);
+	void setValue(const char *key,int value);
+	void setValue(const char *key,double value);
+	void setValue(const char *key,bool value);
+
+	redisType getValue(const char *key);
 private:
 	redisservice();
 	~redisservice();
--- src/redisservice.cpp
+++ src/redisservice.cpp
@@ -19,7 +19,6 @@ redisservice *redisservice::instance()
 	return &obj;
 }
 
-
 redisservice::redisservice()
 {
 	p = new redisserviceParam;
@@ -33,3 +32,98 @@ redisservice::~redisservice()
 	delete p;
 }
 
+void redisservice::setValue(const char *key, const char *value)
+{
+	p->redis->set(key,value);
+}
+
+void redisservice::setValue(const char *key, int value)
+{
+	std::string s_value= std::to_string(value);
+	p->redis->set(key, s_value);
+}
+
+void redisservice::setValue(const char *key, double value)
+{
+	std::string s_value = std::to_string(value);
+	p->redis->set(key, s_value);
+}
+
+void redisservice::setValue(const char *key, bool value)
+{
+	std::string s_value;
+	if (value == true)
+	{
+		s_value = "true";
+	}
+	else
+	{
+		s_value = "true";
+	}
+	p->redis->set(key, s_value);
+}
+
+redisType redisservice::getValue(const char *key)
+{
+	redisType rc;
+	auto name = p->redis->get(key);
+	if (name.has_value()) {
+		std::string name_str = *name;  // 解引用获取 std::string
+		rc.setvalue(name_str);
+	}
+	return rc;
+}
+
+double redisType::toDouble()
+{
+	double rc;
+	if (s_value.empty())
+	{
+		rc=0;
+	}
+	else
+	{
+		rc = std::stod(s_value);
+	}
+	return rc;
+}
+
+int redisType::toInt()
+{
+	int rc;
+	if (s_value.empty())
+	{
+		rc = 0;
+	}
+	else
+	{
+		rc = std::stoi(s_value);
+	}
+	return rc;
+}
+
+std::string redisType::toString()
+{
+	return s_value;
+}
+
+bool redisType::toBool()
+{
+	bool rc=false;
+	if (s_value.empty())
+	{
+		rc = false;
+	}
+	else
+	{
+		if (s_value=="true"|| s_value == "TRUE"|| s_value == "True")
+		{
+			rc = true;
+		}
+		else
+		{
+			rc = false;
+		}
+	}
+	return rc;
+}
